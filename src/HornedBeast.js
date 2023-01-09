94f796ded0b15d027a822b85706e9163c9ccf8b4
Marlon Angulo Jr <marlonangulo001@gmail.com>
2023-01-09 15:54:31 -0500

    start of lab

---
+++ src/HornedBeast.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class HornedBeast extends React.Component{
+  render(){
+    return(
+      <>
+        <h1>Beast {this.props.name}</h1>
+      </>
+    )
+  }
+}
+
+export default HornedBeast;
---
+++ src/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import HornedBeast from './HornedBeast';
+
+class Main extends React.Component{
+  render(){
+    return(
+      <>
+        <h3>Main</h3>
+        <HornedBeast name='Marlon'/>
+      </>
+    )
+  }
+}
+
+export default Main;
