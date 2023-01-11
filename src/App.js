814d62fcfb323173403dfd10a953cbb5d048eeec
Marlon Angulo Jr <marlonangulo001@gmail.com>
2023-01-10 20:51:58 -0500

    finished lab02

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 // import dependencies
 
 import React from 'react';
-import Header from './Header';
-import Main from './Main';
-import Footer from './Footer';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import Footer from './components/Footer/Footer';
 
 // Class component
 
